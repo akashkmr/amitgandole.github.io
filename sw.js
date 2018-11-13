4247a5214dbc0f66c394dbf93d6156f587318368
dhanesh.dhanawade@zconsolutions.com <dhanesh.dhanawade@zconsolutions.com>
2018-11-13 13:16:14 +0530

    Initial commit

--- sw.js
+++ sw.js
@@ -2,3 +2,25 @@
 self.addEventListener('fetch', function(event) {
   /** An empty fetch handler! */
 });
+
+navigator.serviceWorker && navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {  
+const publicKey ='BHlNRf0EG5a7LmaFr0PD6j0kfSZSeVgIS0sJko8xbRpF2U3QsKbp51AtHyNDm_HSyAMR10VEiEj1VckpUXTLSt4';
+  serviceWorkerRegistration.pushManager.getSubscription()  
+    .then(function(subscription) {  
+      if (subscription) {
+        console.info('Got existing', subscription);
+        window.subscription = subscription;
+        return;  // got one, yay
+      }
+
+      const applicationServerKey = urlB64ToUint8Array(publicKey);
+      serviceWorkerRegistration.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey,
+      })
+        .then(function(subscription) { 
+          console.info('Newly subscribed to push!', subscription);
+          window.subscription = subscription;
+        });
+    });
+});
